1eeb02c6f5670b01abe3a5504d08721ff67f02a4
Jakub Sztyma <jakubsztyma@interia.pl>
2019-12-07 20:42:52 +0100

    Format output pgn

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ STOCKFISH_PATH = 'stockfish-10/Linux/stockfish_10_x64'
 
 
 class Solver:
+    CONCISE_HEADERS = {'White', 'Black', 'Site', 'Date'}
+
     def __init__(self, h, cp, d, n, e):
         self.h = h
         self.n = n
@@ -16,13 +18,30 @@ class Solver:
     def __del__(self):
         self.engine.quit()
 
-    def get_output(self, board, moves):
+    def get_desired_headers(self, game_headers):
+        if self.h == 'minimal':
+            return {}
+        elif self.h == 'concise':
+            return {key: val for key, val in game_headers if key in self.CONCISE_HEADERS}
+        elif self.h == 'all':
+            return game_headers
+        else:
+            raise Exception('Invalid h parameter')
+
+
+    def format_game_output(self, default_headers, board):
+        headers = {'fen':board.fen(), **default_headers}
+        game = pgn.Game(headers=headers)
+        return str(game)
+
+    def get_output(self, headers, board, moves):
         # TODO format output accordingly to specification
         result = []
         for move in moves:
             uci_move = move['pv'][0]
             board.push(uci_move)
-            result.append(board.fen())
+            # result.append(board.fen())
+            result.append(self.format_game_output(headers, board))
             board.pop()
         return result
 
@@ -36,13 +55,14 @@ class Solver:
     def handle_game(self, game):
         positions = []
         board = game.board()
+        headers = self.get_desired_headers(game.headers)
 
         for move in game.mainline_moves():
             evaluated_moves = self.engine.analyse(board, self.limit, multipv=self.n)
             best_move, second_best_move = evaluated_moves[:2]
             if self.meets_conditions(best_move, second_best_move):
                 # print(f'Move found {best_move["pv"][0]}')
-                node_output = self.get_output(board, evaluated_moves)
+                node_output = self.get_output(headers, board, evaluated_moves)
                 positions.extend(node_output)
             board.push(move)
         return positions
@@ -58,13 +78,14 @@ def entrypoint(
         e=STOCKFISH_PATH,
         # TODO chess engine params
 ):
+
     print(input_path, output_path, h, cp, d, n, e)  # Show selected params
     solver = Solver(h, cp, d, n, e)
 
     with open(input_path, 'r') as input_, open(output_path, 'w') as output:
         game = pgn.read_game(input_)
         # while game:
-        for _ in range(10):
+        for _ in range(2):
             try:
                 positions = solver.handle_game(game)
                 for position in positions:
